90c1436c46dc65e15fa01e6d385c0255a90f503f
jrogb <grobbelaarjohan65@gmail.com>
2025-07-31 12:49:23 +0000

    Added cards, right bar and transaction pages

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,17 @@
+import Card from '../ui/dashboard/card/card'
+import styles from '../ui/dashboard/card/card.module.css'
+
 const DashboardPage =() => {
     return (
-        <div>Dashboard</div>
+        <div className={styles.wrapper}>
+            <div className={styles.main}>
+                <div className={styles.cards}>
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </div>
+        </div>
     )
 }
 
