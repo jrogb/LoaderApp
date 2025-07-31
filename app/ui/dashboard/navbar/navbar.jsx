5b410745167ec952791f6d655097910d2132be37
jrogb <grobbelaarjohan65@gmail.com>
2025-07-31 05:39:41 +0000

    Structure, sidebar and navbar

---
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -0,0 +1,18 @@
+"use client"
+
+import styles from './navbar.module.css';
+import { usePathname } from 'next/navigation';
+import { MdNotifications, MdOutlineChat, MdPublic, MdSearch } from 'react-icons/md';
+
+const Navbar =() => {
+    const pathname = usePathname();
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>{pathname.split("/").pop()}</div>
+            
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -0,0 +1,111 @@
+import MenuLink from "./menuLink/menuLink";
+import styles from "./sidebar.module.css";
+import Image from "next/image";
+
+import {
+    MdDashboard,
+    MdSupervisedUserCircle,
+    MdShoppingBag,
+    MdAttachMoney,
+    MdWork,
+    MdAnalytics,
+    MdPeople,
+    MdOutlineSettings,
+    MdHelpCenter,
+    MdLogout
+} from "react-icons/md";
+
+const menuItems = [
+    {
+        title:"Pages",
+        list: [
+            {
+                title: "Dashboard",
+                path: "/dashboard",
+                icon: <MdDashboard />
+            },
+            {
+                title: "Users",
+                path: "/users",
+                icon: <MdSupervisedUserCircle />
+            },
+            {
+                title: "Products",
+                path: "/products",
+                icon: <MdShoppingBag />
+            },
+            {
+                title: "Transactions",
+                path: "/transactions",
+                icon: <MdAttachMoney />
+            },
+
+        ],
+    },
+    {
+        title:"Analytics",
+        list: [
+            {
+                title: "Revenue",
+                path: "/revenue",
+                icon: <MdWork />
+            },
+            {
+                title: "Reports",
+                path: "/reports",
+                icon: <MdAnalytics />
+            },
+            {
+                title: "Teams",
+                path: "/teams",
+                icon: <MdPeople />
+            }
+        ],
+    },
+    {
+        title:"User",
+        list: [
+            {
+                title: "Settings",
+                path: "/settings",
+                icon: <MdOutlineSettings />
+            },
+            {
+                title: "Help",
+                path: "/help",
+                icon: <MdHelpCenter />
+            },
+            {
+                title: "Logout",
+                path: "/logout",
+                icon: <MdLogout />
+            }
+        ],
+    },
+];
+
+const Sidebar =() => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.user}>
+                <Image className={styles.userImage} alt="a blank user logo" src="/noavatar.png" width="50" height="50" />
+                <div className={styles.userDetail}>
+                    <span className={styles.username}>John Doe</span>
+                    <span className={styles.userTitle}>Administrator</span>
+                </div>
+            </div>
+           <ul className={styles.list}>
+            {menuItems.map((cat) => (
+                <li key={cat.title}>
+                    <span className={styles.cat}>{cat.title}</span>
+                    {cat.list.map((item) => (
+                        <MenuLink item={item} key={item.title} />
+                    ))}
+                </li>
+            ))}
+           </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
